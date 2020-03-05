ce1b9d1839b20dd0dfb6b5d1219b364ef5646de7
Mysbjorn <48379212+Mysbjorn@users.noreply.github.com>
2020-03-05 08:28:31 +0100

    dialog temp

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : Interactable
+{
+    public GameObject dialogWindow;
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public GameObject continueButton;
+    public override void Interact()
+    {
+        base.Interact();
+        Debug.Log("Talking with me!");
+        StartCoroutine(Type());
+        
+    }
+
+    
+    IEnumerator Type()
+    {
+        Debug.Log("Hello");
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+
+            if (textDisplay.text == sentences[index])
+            {
+                continueButton.SetActive(true);
+            }
+        }
+    }
+
+    public void NextSemtemce()
+    {
+        continueButton.SetActive(false);
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+        }
+    }
+
+}
